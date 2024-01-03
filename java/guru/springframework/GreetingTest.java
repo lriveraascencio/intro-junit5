4c80984bced6a1e402da35ff7aab2987fb3fb11f
Rivera Ascencio <lriveraascencio@deloitte.com>
2024-01-02 19:19:10 -0600

    lifecycle after and before all

--- java/guru/springframework/GreetingTest.java
+++ java/guru/springframework/GreetingTest.java
@@ -1,11 +1,19 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
 	
 	private Greeting greeting;
+	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Before I am only called once!");
+	}
 	
 	@BeforeEach
 	void setUp() {
@@ -24,6 +32,16 @@ class GreetingTest {
 	void testHelloWorldString() {
 		
 		System.out.println(greeting.helloWorld("Luis"));
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("In After Each...");
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("after I am only called once!");
 	}
 
 }
