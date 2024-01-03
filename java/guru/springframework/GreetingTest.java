64c7cc61c485a99f14b3809049af595ac5f1a3d1
Rivera Ascencio <lriveraascencio@deloitte.com>
2024-01-02 19:10:47 -0600

    BeforeEach method called SetUp for test

--- java/guru/springframework/GreetingTest.java
+++ java/guru/springframework/GreetingTest.java
@@ -1,18 +1,28 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
+	
+	private Greeting greeting;
+	
+	@BeforeEach
+	void setUp() {
+		System.out.println("In Before each...");
+		greeting = new Greeting();
+		
+	}
 
 	@Test
 	void testHelloWorld() {
-		Greeting greeting = new Greeting();
+		
 		System.out.println(greeting.helloWorld());
 	}
 
 	@Test
 	void testHelloWorldString() {
-		Greeting greeting = new Greeting();
+		
 		System.out.println(greeting.helloWorld("Luis"));
 	}
 
